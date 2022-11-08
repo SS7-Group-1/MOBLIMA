46b3df7bfe21fa189aaf44adc9d18e846272231c
Jia Wei <32828708+39points@users.noreply.github.com>
2022-11-08 15:17:29 +0800

    Update ShowTimes.java

--- MOBLIMA/App/ShowTimes.java
+++ MOBLIMA/App/ShowTimes.java
@@ -80,6 +80,7 @@ public class ShowTimes {
      * @param showTime - Show time that will be previewed
      */
     public void bookShowtime(ShowTime showTime){
+        System.out.println("=".repeat(40));
         System.out.println("New Booking for " + showTime.getMovie().getTitle()  + (showTime.getMovie().getMovieType() == MovieType.threeD ? " (3D)" : ""));
         Cinemas cinemas = new Cinemas();
         Pricing pricing = new Pricing();
