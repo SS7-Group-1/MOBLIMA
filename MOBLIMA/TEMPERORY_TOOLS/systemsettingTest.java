c39c6264ac51e28cc7647e7944d7e3c1731a64b1
Hasterwfsc <90106147+Hasterwfsc@users.noreply.github.com>
2022-11-08 23:30:36 +0800

    PTSD 101

--- MOBLIMA/TEMPERORY_TOOLS/systemsettingTest.java
+++ MOBLIMA/TEMPERORY_TOOLS/systemsettingTest.java
@@ -1,11 +1,10 @@
 package MOBLIMA.TEMPERORY_TOOLS;
 
+import MOBLIMA.App.ShowTimes;
 import MOBLIMA.App.SystemSetting;
 
 public class systemsettingTest {
     public static void main(String[] args) {
-        SystemSetting test = new SystemSetting();
-        //test.AddHoliday();
-        test.DisplayMenu();
+
     }
 }
